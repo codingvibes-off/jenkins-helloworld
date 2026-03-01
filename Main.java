d281b67e63aa32e479919b3ccc66cb7cd84ccc13
Sullivan Sextius <coding.vibes.officiel@gmail.com>
2026-03-01 14:57:18 +0100

    Update print statement in Main.java with Jenkins message

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, test de scrutation outils de gestion de version!");
+        System.out.println("Hello, test de scrutation outils de gestion de version! modification jenkins");
     }
 }
 
